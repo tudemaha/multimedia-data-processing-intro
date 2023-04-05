10840ffd6e690603802db17ad170054c6dc91e85
tudemaha <gede_mahardika88@yahoo.co.id>
2023-04-05 15:32:40 +0800

    display dataset changes for every step in terminal

--- main.py
+++ main.py
@@ -5,38 +5,51 @@ import tokenizing
 import filtering
 import stemming
 import representations
+import printing
 
 # read the dataset, alay dictionary, and own stopword files
 df = open_file.read_excel('dataset A.xlsx', 101, 110)
 alay_dict = open_file.read_dictionary('alay_dictionary.txt')
 stopwords = open_file.read_stopwords('own_stopword.txt')
 
+# original dataset
+print('Original dataset:\n', df)
+
 # case folding -> lowercase
 case_folding.lowercase(df)
+print('\nLowercase:\n', df)
+
 # case folding -> remove numbers
 case_folding.remove_number(df)
+print('\nNumber removal:\n', df)
+
 #case folding -> remove symbols and emoticons
 case_folding.remove_symbol(df)
+print('\nRemove symbols and emoticons:\n', df)
+
 # case folding -> remove alay words
 case_folding.remove_alay_word(df, alay_dict)
+print('\nAlay words removal:\n', df)
 
 # tokenizing dataset
 dataset_tokenize = tokenizing.tokenizing(df)
+print('\nTokenizing:')
+printing.print_dictionary(dataset_tokenize)
 
 # filter stopwords using nltk, sastrawi, and own stopwords
 filtering.remove_stopwords(dataset_tokenize, stopwords)
+print('\nStopwords removal:')
+printing.print_dictionary(dataset_tokenize)
 
 # nltk stemming
 # stemming.nltk_stemming(dataset_tokenize)
 stemming.sastrawi_stemming(dataset_tokenize)
-
-# print the dictionary
-for key in dataset_tokenize:
-    print(key, dataset_tokenize[key])
+print('\nSastrawi stemming:')
+printing.print_dictionary(dataset_tokenize)
 
 # representation -> one hot encoding (transposed dataframe)
 ohe_df = representations.one_hot_encoding(dataset_tokenize)
-print('\nOne Hot Encoding:\n', ohe_df.T)
+print('\n\nOne Hot Encoding:\n', ohe_df.T)
 # representation -> bag of words (transposed dataframe)
 bow_df = representations.bag_of_words(dataset_tokenize)
 print('\nBag of Words:\n', bow_df.T)
